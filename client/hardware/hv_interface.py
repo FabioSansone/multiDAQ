faa65ff4e251c55262c82e9980dde84be9ef07a8
FabioSansone <fabiosansone33@gmail.com>
2026-05-09 15:34:34 +0200

    Fixed import issues

--- client/hardware/hv_interface.py
+++ client/hardware/hv_interface.py
@@ -1,5 +1,5 @@
 from client.utils.channels import channels_definition
-from hvmodbus import HVModBus
+from client.hardware.hvmodbus import HVModBus
 from client.utils.logger import get_logger
 import time
 from typing import List
